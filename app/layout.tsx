f61257012c7042ca04f3a9dac9b5ece606fe10ee
yadavaman13 <yadavaman8511005211@gmail.com>
2026-07-28 22:08:05 +0530

    navigation and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,17 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav className="bg-gray-500 text-4xl text-blue-50 p-4 flex gap-5">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/project">Project</Link>
+          <Link href="/project/description">Description</Link>
+        </nav>
+        <div className="outline outline-red-400">
+            {children}
+        </div>
+      </body>
     </html>
   );
 }
