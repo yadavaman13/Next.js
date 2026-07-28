6805b12504994b5d8943efff58c6c403a738860f
yadavaman13 <yadavaman8511005211@gmail.com>
2026-07-28 21:47:39 +0530

    routing

---
+++ app/project/description/page.tsx
@@ -0,0 +1,8 @@
+export default function Description(){
+    return(
+        <main>
+            <h1>Project 1</h1>
+            <p>Description.......</p>
+        </main>
+    )
+}
---
+++ app/project/page.tsx
@@ -0,0 +1,8 @@
+export default function Project(){
+    return(
+        <main>
+            <h1>Project page</h1>
+            <p>This is the project page</p>
+        </main>
+    )
+}
