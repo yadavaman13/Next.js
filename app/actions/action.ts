032f0450861c5069d28a569c882bf1eb8224453d
yadavaman13 <yadavaman8511005211@gmail.com>
2026-07-31 17:21:46 +0530

    actions

---
+++ app/actions/action.ts
@@ -0,0 +1,5 @@
+"use server"
+
+export async function Action(){
+    console.log("some actionsssss")
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Card from "@/components/Card"
+import {Action} from "@/app/actions/action"
 
 export default function Home() {
   return (
@@ -8,6 +9,13 @@ export default function Home() {
       <Card title="card 1" description="description of 1st card" />
       <Card title="card 2" description="description of 2nd card" />
       <Card title="card 3" description="description of 3rd card" />
+
+      <button 
+      onClick={Action}
+      className="border-2 border-white text-white p-2.5 rounded-2xl"
+      >
+        click me!
+      </button>
     </>
   );
 }
